1400443fcba6c3b1d127afd2a1a205b7aee6ed70
finalejd <finalejd@gmail.com>
2023-11-27 11:49:14 -0800

    Subida de proyecto

--- frontend/src/api/products.ts
+++
@@ -1,6 +0,0 @@
-import { authAxios, axi } from "./useAxios";
-
-export const get_products =async ({pageParam=1}) => {
-    const response = await axi.get(`/products/?page=${pageParam}&pages=9`)
-    return response.data
-}
